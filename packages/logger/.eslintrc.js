061efcffe2cb1cad3d066f22a54fa4f9c4347d34
Daniel Yoo <daniel@udit.kr>
2025-11-12 17:28:35 +0900

    chore: remove unnecessary files and clean up code

--- packages/logger/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['../../.eslintrc.js'],
-  parserOptions: {
-    project: './tsconfig.eslint.json',
-  },
-  ignorePatterns: ['dist/', 'node_modules/'],
-}
-
